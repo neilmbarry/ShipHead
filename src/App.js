6be36899b490d961f67428b8f39d24b131e3d9af
neilmbarry <neilmbarry@gmail.com>
2022-08-29 17:00:24 -0400

    minor update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import classes from "./App.module.css";
 import Home from "./pages/Home/Home";
 import Lobby from "./pages/Lobby/Lobby";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./pages/Game/Game";
 
 function App() {
   return (
