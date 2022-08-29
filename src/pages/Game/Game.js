1f937abf586bef7d243ff97299cf3b941402342c
neilmbarry <neilmbarry@gmail.com>
2022-08-29 18:01:44 -0400

    experimenting with game grid

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -3,7 +3,15 @@ import classes from "./Game.module.css";
 
 const Game = ({ className }) => {
   const classesList = `${classes.main} ${className}`;
-  return <div className={classesList}>Game</div>;
+  return (
+    <div className={classesList}>
+      <div className={classes.player1}></div>
+      <div className={classes.player2}></div>
+      <div className={classes.player3}></div>
+      <div className={classes.player4}></div>
+      <div className={classes.center}></div>
+    </div>
+  );
 };
 
 export default Game;
