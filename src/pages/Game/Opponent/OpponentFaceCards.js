b82957c2b5ca3ad33785536f0f1ecced4fcf4c09
neilmbarry <neilmbarry@gmail.com>
2022-08-30 09:56:38 -0400

    perfecting opponent table cards

--- src/pages/Game/Opponent/OpponentFaceCards.js
+++ src/pages/Game/Opponent/OpponentFaceCards.js
@@ -6,9 +6,16 @@ const OpponentFaceCards = ({ className }) => {
   const classesList = `${classes.main} ${className}`;
   return (
     <div className={classesList}>
-      <Card className={classes.card} name="AceHearts" />
-      <Card className={classes.card} name="AceSpades" />
-      <Card className={classes.card} name="AceDiamonds" />
+      <div className={classes.faceUp}>
+        <Card className={classes.card} name="AceHearts" />
+        <Card className={classes.card} name="10Spades" />
+        <Card className={classes.card} name="AceDiamonds" />
+      </div>
+      <div className={classes.faceDown}>
+        <Card className={classes.card} name="back" />
+        <Card className={classes.card} name="back" />
+        <Card className={classes.card} name="back" />
+      </div>
     </div>
   );
 };
