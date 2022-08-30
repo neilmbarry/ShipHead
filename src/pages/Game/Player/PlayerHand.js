e26976ee1ba7a4dbea2b936cb097f0a4aff90437
neilmbarry <neilmbarry@gmail.com>
2022-08-30 13:57:50 -0400

    testing full deck in player hand

--- src/pages/Game/Player/PlayerHand.js
+++ src/pages/Game/Player/PlayerHand.js
@@ -1,65 +1,16 @@
 import React from "react";
 import classes from "./PlayerHand.module.css";
 import Card from "../../../components/UI/Card";
+import CardImages from "../../../utils/CardImages";
 
 const PlayerHand = ({ className }) => {
   const classesList = `${classes.main} ${className}`;
+  const fullDeck = Object.keys(CardImages).map((name) => (
+    <Card name={name} className={classes.card} key={name} />
+  ));
   return (
     <div className={classesList}>
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
-      <Card name="3Diamonds" className={classes.card} />
+      {fullDeck}
       <Card name="3Diamonds" className={classes.card} />
     </div>
   );
