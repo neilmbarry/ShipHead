9c50c138110607198377971116f0e732754a0678
neilmbarry <neilmbarry@gmail.com>
2023-03-06 23:22:40 -0500

    removed template

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,16 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userStateTemplate = {
-  name: "Neil",
-  id: "sfji4jije34i3",
-  avatar: "avatar1",
-  notification: {
-    type: "warning",
-    message: "It's not your turn",
-  },
-  selectedCards: [],
-};
-
 const initialState = {
   name: null,
   id: "",
